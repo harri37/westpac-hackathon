58709bd98d0b669b77f4b4ef6b42dd7653c42fa0
harri37 <harrisonwills37@gmail.com>
2024-07-04 15:53:50 +1000

    added page layout for invest

--- frontend/src/app/invest/page.tsx
+++ frontend/src/app/invest/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { Header } from "@westpac/ui/header";
+import { Grid, GridItem } from "@westpac/ui/grid";
 
 export default function Save() {
   return (
@@ -10,7 +11,17 @@ export default function Save() {
         leftIcon="arrow"
         leftOnClick={() => {window.history.back()}}
       />
-      <h1>Save</h1>
+      <Grid>
+        <GridItem rowSpan={3} span={8} className="bg-primary-10 p-2 text-center text-primary">
+          1
+        </GridItem>
+        <GridItem rowSpan={6} span={4} className="bg-primary-10 p-2 text-center text-primary">
+          2
+        </GridItem>
+        <GridItem rowSpan={3} span={8} className="bg-primary-10 p-2 text-center text-primary">
+          3
+        </GridItem>
+      </Grid>
     </>
   );
 }
