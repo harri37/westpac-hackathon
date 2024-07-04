2d2018499f78fa3a0aa66295f7f2a31b6f760a6c
Richard Yu <liam20020303@gmail.com>
2024-07-04 15:44:09 +1000

    made list item

--- frontend/src/app/save/page.tsx
+++ frontend/src/app/save/page.tsx
@@ -2,6 +2,7 @@
 
 import { Header } from "@westpac/ui/header";
 import { Grid, GridItem } from "@westpac/ui/grid";
+import { List, ListItem } from "@westpac/ui/list";
 
 export default function Save() {
   return (
@@ -16,10 +17,10 @@ export default function Save() {
           1
         </GridItem>
         <GridItem rowSpan={6} span={4} className="bg-primary-10 p-2 text-center text-primary">
-          2
+          <List> <ListItem> KFC</ListItem></List>
         </GridItem>
         <GridItem rowSpan={3} span={8} className="bg-primary-10 p-2 text-center text-primary">
-          3
+          3 
         </GridItem>
       </Grid>
     </>
