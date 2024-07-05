ba01ad67a4d2e6492c3f0302a49260b1f153b220
fattyhope <jdinh6@outlook.com>
2024-07-05 13:58:00 +1000

    Landing page

---
+++ frontend/src/app/sections/about.tsx
@@ -0,0 +1,17 @@
+"use client";
+
+import React from 'react';
+
+const About = () => {
+    return (
+
+        <div className="text-center"> {/* Removed text color class from div to apply specific colors to elements inside */}
+            <p className="text-2xl red-text mb-4">
+                Hop In The Future of Banking: Westpac Wizard
+            </p>
+        
+        </div>
+    );
+};
+
+export { About };
